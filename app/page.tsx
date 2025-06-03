8e0c0a94b61a3a8b8882dfe2fad2008d0e0ed53a
HL8 <hl8org@gmail.com>
2025-06-03 18:28:37 +0800

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,24 @@
+"use client"
+
 import { Button } from '@pkg/ui/components/button'
 import React from 'react'
+import { useRouter } from 'next/navigation'
 
 export default function Page() {
+  const router = useRouter()
+
   return (
     <div className="flex items-center justify-center min-h-svh">
       <div className="flex flex-col items-center justify-center gap-4">
         <h1 className="text-2xl font-bold">Hello World</h1>
-        <Button size="sm">Button</Button>
+        <div className="flex flex-row gap-2">
+          <Button size="sm" onClick={() => router.push('/login')}>
+            Login
+          </Button>
+          <Button size="sm" onClick={() => router.push('/dashboard')}>
+            Dashboard
+          </Button>
+        </div>
       </div>
     </div>
   )
